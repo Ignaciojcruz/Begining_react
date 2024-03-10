4d930f67b176a943d7ac4149c205cb8106b3b0cd
Ignacio Cruz <ignaciojcruz@hotmail.com>
2024-03-09 22:10:25 -0300

    Pass Down Multiple Props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,8 +2,15 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 
-export default function App() {
-  return <h1 className='text-lowercase'>Hello World!</h1>
+export default function ParentComponent() {
+  function greetings() {
+    return 'Hello World'
+  }
+
+  return <ChildComponent greetings={greetings} />
   
 }
 
+function ChildComponent(props) {
+  return <p>{props.greetings()}</p>
+}
