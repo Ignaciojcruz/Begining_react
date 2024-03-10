4350e19197f6b8630c231196a6a3515a831226a2
Ignacio Cruz <ignaciojcruz@hotmail.com>
2024-03-09 22:06:56 -0300

    Render a List Using JSX

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,27 +1,29 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-//import './App.css'
 
+export default function App() {
+  const users = [
+    { id: 1, name: 'Nathan', role: 'Web Developer' },
+    { id: 2, name: 'John', role: 'Web Designer' },
+    { id: 3, name: 'Jane', role: 'Team Leader' },
+  ]
 
-export default function ParentComponent() {
   return (
     <>
-      <UserComponent />
-      <ProfileComponent />
-      <FeedComponent />
+      <p>The currently active users list:</p>
+      <ul>
+      {
+        users.map(function(user){
+          // returns Nathan, then John, then Jane
+          return (
+            <li> {user.name} as the {user.role} </li>
+          )
+        })
+      }
+      </ul>
     </>
-  );
+  )
+  
 }
 
-function UserComponent() {
-  return <h1> User Component </h1>;
-}
-
-function ProfileComponent() {
-  return <h1> Profile Component </h1>;
-}
-
-function FeedComponent() {
-  return <h1> Feed Component</h1>;
-}
