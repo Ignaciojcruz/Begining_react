fb5dcc668ca4938e12c1e942d888ffef7e3dd822
Ignacio Cruz <ignaciojcruz@hotmail.com>
2024-03-09 22:08:33 -0300

    Add the Class Attribute

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,27 +3,7 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 
 export default function App() {
-  const users = [
-    { id: 1, name: 'Nathan', role: 'Web Developer' },
-    { id: 2, name: 'John', role: 'Web Designer' },
-    { id: 3, name: 'Jane', role: 'Team Leader' },
-  ]
-
-  return (
-    <>
-      <p>The currently active users list:</p>
-      <ul>
-      {
-        users.map(function(user){
-          // returns Nathan, then John, then Jane
-          return (
-            <li> {user.name} as the {user.role} </li>
-          )
-        })
-      }
-      </ul>
-    </>
-  )
+  return <h1 className='text-lowercase'>Hello World!</h1>
   
 }
 
