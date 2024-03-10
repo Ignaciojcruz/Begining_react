8e897efcb0f549c36ce0226a3348768c3803e754
Ignacio Cruz <ignaciojcruz@hotmail.com>
2024-03-09 22:02:54 -0300

    Return Multiple Elements With Fragments

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 //import './App.css'
+import { Fragment } from 'react'
+
 
 function App() {
   return (
-    <>
+    <Fragment>
       <h1>Hello World</h1>
       <h2>Learning to code with React</h2>
-    </>
+    </Fragment>
   )
 }
 
